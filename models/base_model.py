114c3c1615ce8cd9cb2542a674d2340b5ba38828
kirangagitau <dennosea@gmail.com>
2024-06-26 10:00:37 +0300

    the console task 3

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel:
     def __init__(self):  # constructor to base model
         self.id = str(uuid.uuid4())
 
-        self.created_at_ = datetime.utcnow()
-        self.updated_at_ = datetime.utcnow()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def save(self):
         """
@@ -30,14 +30,28 @@ class BaseModel:
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
 
-        return inst_dict()
+        return inst_dict
 
     def __str__(self):
         """
         method for string representation of class
         """
-        class_name = self.__class__.name__
+        class_name = self.__class__.__name__
         """
         return a formated string for
         """
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
