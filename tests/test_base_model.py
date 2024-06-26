74da5ebbdd0607699c710176322554aadb85f620
kirangagitau <dennosea@gmail.com>
2024-06-26 11:12:04 +0300

    the console task 3

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,47 +1,54 @@
 #!/usr/bin/python3
-"""
-use python version 3
-this is testing for proper functionality of 
-the base_model class
-"""
 
 import unittest
-from models.base_model import BaseModel
+from datetime import datetime, timedelta
+from models.basemodel import BaseModel
 
-class TestBasemodel(unittest.TestCase):
-    def test_init(self):  # testing the constructor.
-        my_nodel = Basemodel()
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        """
+        initializes a BaseModel instance before each test,
+        rather than repetition
+        """
+        self.model = BaseModel()
 
-        self.assertIsNotNone(my_model.id)  # be true
-        self.assertIsNotNone(my_model.created_at)  # be true
-        self.assertIsNotNone(my_model.updated_at)  # be true
+    def test_initialization(self):
+        """
+        checks that objects are equal initially
+        """
+        self.assertIsInstance(self.model.id, str)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
+        self.assertEqual(self.model.created_at, self.model.updated_at)
 
-    def test_save(self):  # testing save method.
-        my_model = BaseModel()
-
-        init_updated_at = my_model.updated_at
-
-        new_updated_at = my_model.save()
-
-        self.assertNotEwual(init_updated_at, new_updated_at)
+    def test_save_method(self):
+        """
+        Ensures that updated_at is updated when save is called
+        """
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        new_updated_at = self.model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+        self.assertTrue(new_updated_at > old_updated_at)
 
     def test_to_dict(self):
-        my_model = BaseModel()
+        """
+        Verifies that the dictionary representation includes the correct
+        values & formats
+        """
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+        self.assertEqual(model_dict["created_at"], self.model.created_at.isoformat())
+        self.assertEqual(model_dict["updated_at"], self.model.updated_at.isoformat())
 
-        my_model_dict = my_model.to_dict()
+    def test_str_method(self):
+        """
+        confirms the string representation of the basemodel
+        """
+        model_str = str(self.model)
+        expected_str = "[BaseModel] ({}) {}".format(self.model.id, self.model.__dict__)
+        self.assertEqual(model_str, expected_str)
 
-        self.assertIsInstance(my_model_dict, dict)  # should be dict
-        self.assertEqual(my_model_dict["__class__", 'BaseModel'])  # correct key
-        self.assertEqual(my_model_dict['id'], my_model.id)
-        self.assertEqual(my_model_dict['created_at'], my_model_at.isoformat())
-        self.assertEqual(my_model_dict["updated_at"], my_model.created_at.isoformat())
-
-    def test_str(self):
-        my_model = BaseModel()
-
-        self.assertTrue(str(my_model).startWith(['BaseModel']))
-        self.assertIn(my_model.id, str(my_model))
-        self.assertIn(str(my_model.__dict__), str(my_model))
-
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
