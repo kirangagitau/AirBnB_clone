d2422362e75b5838b28256c3f7afe264cba96eb0
kirangagitau <dennosea@gmail.com>
2024-07-18 18:20:36 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -24,18 +24,4 @@ class BaseModel:
 
     def __str__(self):
         """Returns the string representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-# Example usage
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)            # Output the string representation of the model
-    my_model.save()            # Update the updated_at attribute
-    print(my_model)            # Output the updated string representation of the model
-    my_model_json = my_model.to_dict()  # Get the dictionary representation of the model
-    print(my_model_json)       # Print the dictionary representation
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dic__)
